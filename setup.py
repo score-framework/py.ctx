fb3f9fbb9004b5c2f3b9fefbd996661464437f57
Necdet Can Ateşman <can@atesman.at>
2020-04-23 14:38:17 +0200

    Release: 0.6.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.ctx',
-    version='0.6.0',
+    version='0.6.1',
     description='Context handling for The SCORE Framework',
     long_description=README,
     author='strg.at',
